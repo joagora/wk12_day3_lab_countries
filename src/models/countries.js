601819d2ae9aceb843394b44bb6f7272a0267324
Tahnee Downie <tahneedownie@yahoo.com>
2018-09-12 13:51:15 +0100

    Initial setup.

---
+++ src/models/countries.js
@@ -0,0 +1,17 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Countries = function () {
+  this.data = null;
+}
+
+Countries.prototype.getData = function () {
+  const request = new Request('https://restcountries.eu/rest/v2/all');
+  request.get((data) => {
+    this.data = data;
+    PubSub.publish('Countries:data-ready');
+
+  })
+}
+
+module.exports = Countries;
