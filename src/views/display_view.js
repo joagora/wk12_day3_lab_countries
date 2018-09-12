39d5571075508640c6badc8e701829e5c723ddbd
Tahnee Downie <tahneedownie@yahoo.com>
2018-09-12 15:20:36 +0100

    display languages

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -12,10 +12,33 @@ DisplayView.prototype.bindEvents = function () {
 
 DisplayView.prototype.displayData = function(countryToDisplay) {
   const countryDiv = document.querySelector('#country');
+  countryDiv.textContent = "";
   const nameHeader = document.createElement('h1');
   countryDiv.appendChild(nameHeader);
   nameHeader.textContent = countryToDisplay.name;
 
+  const imageDiv = document.createElement('div');
+  countryDiv.appendChild(imageDiv);
+  imageDiv.setAttribute('id', 'flag-image');
+  imageDiv.setAttribute('style', `background-image: url(${countryToDisplay.flag})`);
+
+  const regionHeader = document.createElement('h3');
+  countryDiv.appendChild(regionHeader);
+  regionHeader.textContent = countryToDisplay.region;
+
+  const populationHeader = document.createElement('h4');
+  countryDiv.appendChild(populationHeader);
+  populationHeader.textContent = `population: ${countryToDisplay.population}`;
+
+  const languagesUl = document.createElement('ul');
+  countryDiv.appendChild(languagesUl);
+  const countryLanguages = countryToDisplay.languages;
+  for(countryLanguage of countryLanguages) {
+    const languageLi = document.createElement('li');
+    languagesUl.appendChild(languageLi);
+    languageLi.textContent = countryLanguage.name;
+  }
+  console.log(countryLanguages);
 }
 
 
